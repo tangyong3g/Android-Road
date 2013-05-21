768d3512a23b1ae18d0e7f9ff9a6fc7a2296bb79
tangyong3g <ty_sany@163.om>
2013-05-22 00:00:01 +0800

    其它: GlSurfaceView上次没有正常的上传，使工程错误。

---
+++ src/com/ty/exsample_unit_4/SurfaceViewTest.java
@@ -0,0 +1,12 @@
+package com.ty.exsample_unit_4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SurfaceViewTest extends Activity{
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
