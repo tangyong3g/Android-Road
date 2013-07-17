91ebaae9e02f3d276f464d0381f5b2356b5c9c31
unknown <Administrator@.(none)>
2013-07-18 00:35:04 +0800

    第四单元: 为Looper添加说明。

--- src/com/ty/exsample_unit_4/LooperActivity.java
+++ src/com/ty/exsample_unit_4/LooperActivity.java
@@ -22,6 +22,15 @@ import com.example.android_begin_gl_3d.R;
  * 
  * 实现主线程给子线程发送消息，结果是在Log里面可以看到在子线程里面处理消息。
  * 
+ * 关于Looper的知识，
+ * 引用 http://www.cnblogs.com/ctou45/archive/2012/01/06/2314191.html
+ * 
+ * 1: Looper类用来为一个线程开启一个消息循环。
+ * 2: 通常是通过Handler对象来与Looper进行交互的。Handler可看做是Looper的一个接口，用来向指定的Looper发送消息及定义处理方法。 
+ * 3: 在非主线程中直接new Handler() 会报如下的错误:
+ * 4:  Looper.loop(); 让Looper开始工作，从消息队列里取消息，处理消息。
+ * 5:  基于以上知识，可实现主线程给子线程（非主线程）发送消息。 [也就是本例子中所实现的功能]
+ * 
  * 
  * 
  * @author 师爷GBK[ty_sany@163.com] 2013-7-17
