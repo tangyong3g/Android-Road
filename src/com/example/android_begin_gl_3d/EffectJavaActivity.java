393db82f7cb597f9175353f1865f7f78f4646285
tangyong3g <ty_sany@163.com>
2014-05-07 20:57:05 +0800

    第8单元　开始初始化

---
+++ src/com/example/android_begin_gl_3d/EffectJavaActivity.java
@@ -0,0 +1,19 @@
+package com.example.android_begin_gl_3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 第7单元
+ * 
+ * @author tang
+ *
+ */
+public class EffectJavaActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
--- src/com/example/android_begin_gl_3d/MainActivity.java
+++ src/com/example/android_begin_gl_3d/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends ListActivity {
 
 	String[] units = new String[] { "unit_1", "unit_2[OpenGL1.x/2.x]",
 			"unit_3[LibGDX]", "unit_4[Android基本知识]", "unit_5[Android游戏开发案例]",
-			"unit_6[重用组件]", "unit_7[Shell Engine]", };
+			"unit_6[重用组件]", "unit_7[Shell Engine]","unit_8[EffectJava]" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +73,9 @@ public class MainActivity extends ListActivity {
 		case 6:
 			cls = Main.class;
 			break;
+		case 7:
+			
+			cls = EffectJavaActivity.class;
 
 		default:
 			break;
