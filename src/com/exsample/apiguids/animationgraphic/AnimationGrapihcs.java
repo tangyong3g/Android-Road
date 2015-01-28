9c9995fd9f76cdb228408750102a13dabc453bc9
tangyong3g <ty_sany@163.com>
2015-01-28 10:39:06 +0800

    API guids
    load image effeciently !

--- src/com/exsample/apiguids/animationgraphic/AnimationGrapihcs.java
+++ src/com/exsample/apiguids/animationgraphic/AnimationGrapihcs.java
@@ -1,5 +1,7 @@
 package com.exsample.apiguids.animationgraphic;
 
+import java.lang.ref.WeakReference;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
@@ -7,9 +9,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.ty.exsample.R;
@@ -35,8 +39,7 @@ import com.ty.exsample.R;
 public class AnimationGrapihcs extends Activity {
 
 	LinearLayout mContainer;
-	
-	
+
 	LoadLargeBitmapEfficently mBitMapEfficently;
 
 	@Override
@@ -45,26 +48,25 @@ public class AnimationGrapihcs extends Activity {
 
 		initView();
 	}
-	
-	
-	
-	private void initView(){
-			
-		mContainer  = new LinearLayout(this.getApplicationContext());
-		mBitMapEfficently = new LoadLargeBitmapEfficently(this.getApplicationContext());
-		
-		mContainer.addView(mBitMapEfficently);
-		
-		
-		setContentView(mContainer);
-	}
-	
-	
-	
-	
-/********************************************************* display bitmap effeciently ********************************************************************************/
 
 	
+	/**
+	 * <br>功能简述:
+	 * <br>功能详细描述:
+	 * <br>注意:
+	 */
+	private void initView() {
+
+		mContainer = new LinearLayout(this.getApplicationContext());
+		mBitMapEfficently = new LoadLargeBitmapEfficently(this.getApplicationContext());
+
+		mContainer.addView(mBitMapEfficently);
+
+		setContentView(mContainer);
+	}
+
+	/********************************************************* display bitmap effeciently ********************************************************************************/
+
 	/**
 	 * 
 	 * <br>类描述:
@@ -77,9 +79,8 @@ public class AnimationGrapihcs extends Activity {
 
 		final String TAG = "LoadLargeBitmapEfficently";
 		Bitmap mBitmap;
-		Paint mPaint ;
-		
-		
+		Paint mPaint;
+
 		/**
 		 * 
 		 * <br>功能简述: load bitmap Demesions and type
@@ -99,34 +100,29 @@ public class AnimationGrapihcs extends Activity {
 			String type = ops.outMimeType;
 
 			Log.i(TAG, "width:\t" + width + "height:\t" + height + "type:\t" + type);
-
 		}
-		
 
+		
 		public LoadLargeBitmapEfficently(Context context) {
 			super(context);
-			
+
 			mPaint = new Paint();
 			mBitmap = decodeSampledBitmapFromResource(getResources(), R.drawable.bg_one, 100, 100);
 			loadBitmapDemisionAndType();
-			
+
 		}
 
 		@Override
 		protected void onDraw(Canvas canvas) {
 			super.onDraw(canvas);
-			
-			if ( mBitmap!=null ) {
-				canvas.drawBitmap(mBitmap, 0, 0 ,mPaint);
+
+			if (mBitmap != null) {
+				canvas.drawBitmap(mBitmap, 0, 0, mPaint);
 			}
-			
+
 		}
 
 	}
-	
-	
-	
-	
 
 	/**
 	 * <br>功能简述:
@@ -158,8 +154,7 @@ public class AnimationGrapihcs extends Activity {
 
 		return inSampleSize;
 	}
-	
-	
+
 	/**
 	 * <br>功能简述:
 	 * <br>功能详细描述:
@@ -170,21 +165,61 @@ public class AnimationGrapihcs extends Activity {
 	 * @param reqHeight
 	 * @return
 	 */
-	public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-	        int reqWidth, int reqHeight) {
+	public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
 
-	    // First decode with inJustDecodeBounds=true to check dimensions
-	    final BitmapFactory.Options options = new BitmapFactory.Options();
-	    options.inJustDecodeBounds = true;
-	    BitmapFactory.decodeResource(res, resId, options);
+		// First decode with inJustDecodeBounds=true to check dimensions
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeResource(res, resId, options);
 
-	    // Calculate inSampleSize
-	    options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+		// Calculate inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
 
-	    // Decode bitmap with inSampleSize set
-	    options.inJustDecodeBounds = false;
-	    return BitmapFactory.decodeResource(res, resId, options);
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeResource(res, resId, options);
 	}
+
+	/********************************************************* process Bitmap in UI Thread  ___ load Bitmap AsyncTask  ******************************************************/
+
 	
+	/**
+	 * 
+	 * <br>类描述:
+	 * <br>功能详细描述:
+	 * 
+	 * 
+	 * @author  tangyong
+	 * @date  [2015-1-28]
+	 */
+	class BitmapWorkTask extends AsyncTask<Integer, Void, Bitmap> {
+
+		private final WeakReference<ImageView> mWeakReferenceBitmap;
+		private int data = 0;
+
+		public BitmapWorkTask(ImageView bitmap) {
+			mWeakReferenceBitmap = new WeakReference<ImageView>(bitmap);
+		}
+
+		@Override
+		protected Bitmap doInBackground(Integer... params) {
+
+			int data = params[0];
+
+			return decodeSampledBitmapFromResource(getResources(), data, 100, 100);
+
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			super.onPostExecute(result);
+
+			if (result == null && mWeakReferenceBitmap == null) {
+				ImageView img = mWeakReferenceBitmap.get();
+				img.setImageBitmap(result);
+			}
+		}
+
+	}
 
 }
