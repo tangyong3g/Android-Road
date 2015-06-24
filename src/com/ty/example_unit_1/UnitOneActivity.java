ca7244e9d1bef6fdc7d54ada0e1123cea10548d5
tangyong3g <ty_sany@163.comj>
2015-06-24 17:32:53 +0800

    其它 测试Next版本的问题。

--- src/com/ty/example_unit_1/UnitOneActivity.java
+++ src/com/ty/example_unit_1/UnitOneActivity.java
@@ -1,14 +1,21 @@
 package com.ty.example_unit_1;
 
+import java.io.File;
+import java.util.List;
+
 import com.ty.exsample.R;
 import com.ty.example_unit_1.coordiatesystem.CoordiateSystemActivity;
 import com.ty.example_unit_1.cube.CubeActivity;
 import com.ty.example_unit_1.service.ServiceActivity;
+import com.ty.util.MD5Util;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.ArrayAdapter;
@@ -68,8 +75,43 @@ public class UnitOneActivity extends ListActivity {
 	private void intentToActivity(Class cls) {
 		Intent intent = new Intent();
 		intent.setClass(this, cls);
-
 		startActivity(intent);
+		getApplicationMD5(getApplicationContext());
 	}
+	
+
+	/**
+	 * <br>
+	 * 功能简述:获取应用程序的md5 <br>
+	 * 功能详细描述: <br>
+	 * 注意:
+	 * 
+	 * @return
+	 */
+	public String getApplicationMD5(Context context) {
+		String md5String = null;
+		Intent intent = new Intent("android.intent.action.MAIN");
+		intent.addCategory("android.intent.category.HOME");
+		intent.addCategory("android.intent.category.DEFAULT");
+
+		List<ResolveInfo> pkgAppsList = context.getPackageManager().queryIntentActivities(intent, 0);
+		try {
+			for (ResolveInfo info : pkgAppsList) {
+
+				String pkgNameString = "com.gtp.nextlauncher";
+
+				if (info.activityInfo.packageName.equals(pkgNameString)) {
+					File file = new File(info.activityInfo.applicationInfo.sourceDir);
+					md5String = MD5Util.getFileMD5String(file);
+				}
+			}
+		} catch (Exception e) {
+			md5String = null;
+		}
+
+		Log.i("test", "MD5 ID " + md5String);
+		return md5String;
+	}
+
 
 }
--- src/com/ty/example_unit_1/service/ServiceActivity.java
+++ src/com/ty/example_unit_1/service/ServiceActivity.java
@@ -1,41 +1,48 @@
 package com.ty.example_unit_1.service;
 
+import java.io.File;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.ty.util.MD5Util;
+
 /**
  * 
  * @author tangyong
  * 
  */
-public class ServiceActivity extends Activity implements OnClickListener{
-	
-	private boolean mFlag = false; 
-	
-	
+public class ServiceActivity extends Activity implements OnClickListener {
+
+	private boolean mFlag = false;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Button btn  = new Button(this);
+		Button btn = new Button(this);
 		btn.setOnClickListener(this);
 		setContentView(btn);
+
 	}
 
 	@Override
 	public void onClick(View v) {
-		Log.i("tyler.tang","线程编号:\t"+Thread.currentThread().getId());
-		mFlag = !mFlag ;
-		
+		Log.i("tyler.tang", "线程编号:\t" + Thread.currentThread().getId());
+		mFlag = !mFlag;
+
 		Intent intent = new Intent();
-		intent.setClassName(this,MyService.class.getName());
+		intent.setClassName(this, MyService.class.getName());
 		intent.putExtra("state", mFlag);
-		
+
 		startService(intent);
 	}
-	
+
 }
