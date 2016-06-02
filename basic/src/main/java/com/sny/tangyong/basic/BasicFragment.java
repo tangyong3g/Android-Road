540972acd6d7aa716317f9b3a67d7915f9b9cbaf
tang <ty_sany@163.com>
2016-06-03 02:28:49 +0800

    android basic fragment

--- basic/src/main/java/com/sny/tangyong/basic/BasicFragment.java
+++ basic/src/main/java/com/sny/tangyong/basic/BasicFragment.java
@@ -7,13 +7,16 @@ import android.support.v4.app.FragmentActivity;
 /**
  * Created by Administrator on 2016/6/3.
  */
-public class BasicFragment extends FragmentActivity {
+public class BasicFragment extends FragmentActivity implements HeadFragment.IFragmentTransToActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.fragmentactivity);
+    }
+
+    @Override
+    public void translateState(String state) {
 
     }
 }
--- basic/src/main/java/com/sny/tangyong/basic/HeadFragment.java
+++ basic/src/main/java/com/sny/tangyong/basic/HeadFragment.java
@@ -1,5 +1,6 @@
 package com.sny.tangyong.basic;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,6 +12,25 @@ import android.view.ViewGroup;
  * Created by Administrator on 2016/6/3.
  */
 public class HeadFragment extends Fragment {
+
+    IFragmentTransToActivity mIfragmentTransTOActivity;
+
+
+    public interface IFragmentTransToActivity{
+        void translateState(String state);
+
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try{
+            mIfragmentTransTOActivity = (IFragmentTransToActivity)activity;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
 
     @Nullable
     @Override
