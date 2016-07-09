43e7c2ac8f0b4639105cb5a8b86099a83fbe9da7
tangyong <ty_sany@163.com>
2016-07-09 16:13:59 +0800

    temp

--- graphicengine/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-#APP_ABI := all
-#APP_ABI := armeabi
-#APP_ABI := x86
-APP_ABI := armeabi armeabi-v7a x86
-#APP_ABI := mipsel
-#mips平台单独进行编译
-#APP_ABI := mips
--- graphicengine/src/com/graphics/engine/badlogic/gdx/Disposable.java
+++
@@ -1,9 +0,0 @@
-package com.graphics.engine.badlogic.gdx;
-
-/** Interface for disposable resources.
- * @author mzechner */
-//CHECKSTYLE IGNORE 10000 LINES
-public interface Disposable {
-	/** Releases all resources of this object. */
-	public void dispose ();
-}
